7c1066979fa81916de1ead61f07b5941d3603cf8
David Corvoysier <david.corvoysier@gmail.com>
2018-05-16 09:09:59 +0200

    run.sh: allow script to be launched from any directory

--- run.sh
+++ run.sh
@@ -1,4 +1,13 @@
-cd cc
-./build.sh
-cd ..
-PYTHONPATH=.:${PYTHONPATH} python3 bin/digit_binarized_quantized_weights.py
+#!/bin/bash 
+# Absolute path to this script, e.g. /home/user/bin/foo.sh
+THISSCRIPT=$(readlink -f "$0")
+# Absolute path this script is in, thus /home/user/bin
+THISPATH=$(dirname "$THISSCRIPT")
+
+# Build custom ops
+make -C ${THISPATH}/cc
+
+# Launch command from script directory since we use relative path when
+# loading binary ops
+cd ${THISPATH}
+PYTHONPATH=${THISPATH}:${PYTHONPATH} python3 ${THISPATH}/bin/digit_binarized_quantized_weights.py
